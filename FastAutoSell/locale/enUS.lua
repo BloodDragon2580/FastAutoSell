9a92e42bb1de4bc69103da2b2fa185d1def68365
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-12-31 12:19:18 +0100

    Update to v2.3

--- FastAutoSell/locale/enUS.lua
+++ FastAutoSell/locale/enUS.lua
@@ -1,3 +1,3 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FastAutoSell", "enUS", true)
 
-L["Fast Auto Repair"] = "|cffd6266cFastAutoSell:|cffffcc00Items were sold for "
+L["FastAutoSellr"] = "|cffd6266cFastAutoSell:|cffffcc00Items were sold for "
--- FastAutoSell/locale/ruRU.lua
+++ FastAutoSell/locale/ruRU.lua
@@ -1,4 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FastAutoSell", "ruRU")
 if not L then return end
 
-L["Fast Auto Repair"] = "|cffd6266cFastAutoSell:|cffffcc00ЭТовары были проданы за "
+L["FastAutoSell"] = "|cffd6266cFastAutoSell:|cffffcc00ЭТовары были проданы за "
