6bbe139a888214d4841c77ca478ffd23d0b2a86a
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-12-31 16:10:10 +0100

    Update to v2.4
    fix typo

--- FastAutoSell/locale/enUS.lua
+++ FastAutoSell/locale/enUS.lua
@@ -1,3 +1,3 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("FastAutoSell", "enUS", true)
 
-L["FastAutoSellr"] = "|cffd6266cFastAutoSell:|cffffcc00Items were sold for "
+L["FastAutoSell"] = "|cffd6266cFastAutoSell:|cffffcc00Items were sold for "
